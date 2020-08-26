598680ef348bc9e536eae393a788668b992207fa
Daniel Rojas <daniel.drojas@outlook.com>
2020-08-26 15:55:46 -0400

    Updates

---
+++ precache-manifest.44e174d1f08586ac1685a4317a11d0b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4f4687023ca973173f3afaac8f5c43",
+    "url": "/react-domain/index.html"
+  },
+  {
+    "revision": "ae9cb0feaeccbd6c3f44",
+    "url": "/react-domain/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "778fbbea9376b2c81e75",
+    "url": "/react-domain/static/js/2.3bceab16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-domain/static/js/2.3bceab16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9cb0feaeccbd6c3f44",
+    "url": "/react-domain/static/js/main.fb82c026.chunk.js"
+  },
+  {
+    "revision": "7204263e6240511521d1",
+    "url": "/react-domain/static/js/runtime-main.6fa39a46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-domain/static/media/logo.5d5d9eef.svg"
+  }
+]);
